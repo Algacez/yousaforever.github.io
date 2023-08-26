1fb1b7740b577d2f4183eabc086c760313584df8
yousaforever <3624570292@qq.com>
2023-08-26 12:24:39 +0800

    Site updated: 2023-08-26 12:24:38

--- js/utils.js
+++ js/utils.js
@@ -871,7 +871,7 @@ const anzhiyu = {
       return;
     }
     const urlParams = new URLSearchParams(window.location.search);
-    const userId = "8152976493";
+    const userId = "168314790";
     const userServer = "netease";
     const anMusicPageMeting = document.getElementById("anMusic-page-meting");
     if (urlParams.get("id") && urlParams.get("server")) {
